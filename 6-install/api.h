a6c6704a3f1d2814fc52d40102486439df24dcf0
goolyuyi <goolyuyi@hotmail.com>
2023-02-15 22:55:46 +0800

    6

---
+++ 6-install/api.h
@@ -0,0 +1,14 @@
+//
+// Created by 于一 on 2023/2/15.
+//
+
+#ifndef CMAKEPLAYGROUND_API_H
+#define CMAKEPLAYGROUND_API_H
+
+#include <iostream>
+
+void printAPIVersion() {
+    std::cout << "API version: 1.0.0" << std::endl;
+}
+
+#endif //CMAKEPLAYGROUND_API_H
--- 6-install/some_app_consume_the_install.cpp
+++ 6-install/some_app_consume_the_install.cpp
@@ -1,18 +1,22 @@
 //
 // Created by 于一 on 2023/2/15.
 //
+//
+// Created by 于一 on 2023/2/15.
+//
 
 #include <iostream>
 #include "api.h"
 #include "dog-lib.h"
 
 int main(int argc, char *argv[]) {
+    int a;
     using namespace std;
-    cout << "welcome" << endl;
+    cout << "some_app_consume_the_install" << endl;
 
     printAPIVersion();
+    void dog();
 
-    dog();
 
     return 0;
 }
